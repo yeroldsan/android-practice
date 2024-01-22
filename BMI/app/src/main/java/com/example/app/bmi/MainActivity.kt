1f2d5dcc4d0195c60d4a1238110f76b32bf20e94
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2024-01-22 20:35:28 +0200

    feat: Add core logic to calculate the BMI

--- BMI/app/src/main/java/com/example/app/bmi/MainActivity.kt
+++ BMI/app/src/main/java/com/example/app/bmi/MainActivity.kt
@@ -3,13 +3,32 @@ package com.example.app.bmi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.app.bmi.ui.theme.BMITheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Bmi()
                 }
             }
         }
@@ -30,17 +49,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Bmi() {
+    var heightInput: String by remember {
+        mutableStateOf("")
+    }
+    var weightInput: String by remember {
+        mutableStateOf("")
+    }
+    val height = heightInput.toFloatOrNull() ?: 0.0f
+    val weight = weightInput.toIntOrNull() ?: 0
+    val bmi =
+        if (weight > 0 && height > 0) weight / (height * height) else 0.0
+
+    
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun BmiPreview() {
     BMITheme {
-        Greeting("Android")
+        Bmi()
     }
 }
