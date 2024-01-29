b94ca4e60f80066183e084740373ee395fbc6de0
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2024-01-29 10:42:49 +0200

    Initialize Login Form Page app

---
+++ LoginForm/app/src/main/java/com/example/app/loginform/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.app.loginform
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.app.loginform.ui.theme.LoginFormTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginFormTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LoginFormTheme {
+        Greeting("Android")
+    }
+}
