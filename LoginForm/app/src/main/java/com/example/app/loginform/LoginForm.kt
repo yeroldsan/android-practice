8501f1c40ab985339636d53618ee44f2afc78e4f
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2024-01-30 19:30:12 +0200

    feat(validation): Add basic email and password validation

--- LoginForm/app/src/main/java/com/example/app/loginform/LoginForm.kt
+++ LoginForm/app/src/main/java/com/example/app/loginform/LoginForm.kt
@@ -1,10 +1,12 @@
 package com.example.app.loginform
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.text.KeyboardActions
@@ -12,10 +14,15 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.outlined.Visibility
+import androidx.compose.material.icons.outlined.VisibilityOff
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,7 +31,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.app.loginform.ui.theme.LoginFormTheme
@@ -32,17 +43,28 @@ import com.example.app.loginform.ui.theme.LoginFormTheme
 @Composable
 fun RegistrationForm(modifier: Modifier = Modifier) {
     var email by remember { mutableStateOf("") }
+    var isValidEmail by remember { mutableStateOf(true) }
     var password by remember { mutableStateOf("") }
-    // var passwordHidden by remember { mutableStateOf(true) }
-    var isError by remember{ mutableStateOf(false) }
-    val charLimit = 8
+    var isValidPassword by remember{ mutableStateOf(true) }
+    var passwordHidden by remember { mutableStateOf(true) }
 
-    fun validatePassword(text: String) {
-        isError = text.length < charLimit
+    fun validateEmail(email: String): Boolean {
+        val emailRegex = Regex("^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,})+\$")
+        return email.matches(emailRegex)
     }
 
-    val onUserNameChange: (String) -> Unit = { email = it }
-    val onPasswordChange: (String) -> Unit = { password = it }
+    fun validatePassword(pass: String): Boolean {
+        return pass.isNotEmpty() && pass.length >= 8
+    }
+
+    val onEmailChange: (String) -> Unit = {
+        email = it
+        isValidEmail = validateEmail(it)
+    }
+    val onPasswordChange: (String) -> Unit = {
+        password = it
+        isValidPassword = validatePassword(it)
+    }
 
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -65,13 +87,27 @@ fun RegistrationForm(modifier: Modifier = Modifier) {
             Icon(
                 imageVector = Icons.Outlined.Email,
                 contentDescription = null,
+                tint = if (isValidEmail) MaterialTheme.colorScheme.primary else Color.Black,
                 modifier = Modifier.padding(horizontal = 10.dp)
             )
             OutlinedTextField(
                 value = email,
-                onValueChange = onUserNameChange,
+                onValueChange = onEmailChange,
                 label = { Text(text = "Email") },
+                isError = !isValidEmail,
                 singleLine = true,
+                supportingText = {
+                    Text(text = if (!isValidEmail) "Invalid email address" else "")
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    autoCorrect = false,
+                    // capitalization = KeyboardOptions.Capitalization.None,
+                    imeAction = ImeAction.Next,
+                ),
+                keyboardActions = KeyboardActions {
+                    validateEmail(email)
+                }
             )
         }
        Row (
@@ -83,6 +119,7 @@ fun RegistrationForm(modifier: Modifier = Modifier) {
            Icon(
                imageVector = Icons.Outlined.Lock,
                contentDescription = null,
+               tint = if (isValidEmail) MaterialTheme.colorScheme.primary else Color.Black,
                modifier = Modifier.padding(horizontal = 10.dp)
            )
            OutlinedTextField(
@@ -90,23 +127,36 @@ fun RegistrationForm(modifier: Modifier = Modifier) {
                onValueChange = onPasswordChange,
                label = { Text(text = "Password") },
                singleLine = true,
-               isError = isError,
-               keyboardActions = KeyboardActions { validatePassword(password) },
+               supportingText = {
+                   Text(text = if (!isValidPassword) "Password must be at least 8 characters" else "")
+               },
+               isError = !isValidPassword,
                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-//               visualTransformation =
-//                    if (passwordHidden) PasswordVisualTransformation()
-//                    else VisualTransformation.None,
-//               trailingIcon = {
-//                   IconButton(onClick = { passwordHidden = !passwordHidden }) {
-//                       val visibilityIcon =
-//                           if (passwordHidden) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
-//                       // Please provide localized description for accessibility services
-//                       val description = if (passwordHidden) "Show password" else "Hide password"
-//                       Icon(imageVector = visibilityIcon, contentDescription = description)
-//                   }
-//               }
+               keyboardActions = KeyboardActions { validatePassword(password) },
+               visualTransformation =
+                    if (passwordHidden) PasswordVisualTransformation()
+                    else VisualTransformation.None,
+               trailingIcon = {
+                   val visibilityIcon = // Change icon based on password visibility state
+                       if (passwordHidden) Icons.Outlined.Visibility else Icons.Outlined.VisibilityOff
+                   // Localized description for accessibility services
+                   val description = if (passwordHidden) "Show password" else "Hide password"
+
+                   IconButton(
+                       onClick = { passwordHidden = !passwordHidden }
+                   ) {
+                       Icon(imageVector = visibilityIcon, contentDescription = description)
+                   }
+               }
            )
        }
+        Button(
+            onClick = { /*TODO*/ },
+            enabled = isValidEmail && isValidPassword,
+            // modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp),
+        ) {
+            Text(text = "Login")
+        }
     }
 }
 
@@ -115,8 +165,10 @@ fun LoginCard() {
     Card (
         modifier = Modifier
             .fillMaxWidth()
-            .heightIn(max = 500.dp)
-            .paddingFromBaseline(bottom = 0.dp)
+            //.align(Alignment.BottomCenter),
+//            .heightIn(max = 500.dp)
+//            .paddingFromBaseline(bottom = 0.dp)
+//            .padding(horizontal = 16.dp)
     ) {
         RegistrationForm()
     }
@@ -125,7 +177,9 @@ fun LoginCard() {
 @Preview(showBackground = true)
 @Composable
 fun RegistrationFormPreview() {
-    LoginCard()
+    Surface {
+        LoginCard()
+    }
     LoginFormTheme {
         // LoginCard()
     }
