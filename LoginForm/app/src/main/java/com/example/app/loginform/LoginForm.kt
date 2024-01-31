2c12800fc7aa2546df07ed5bbe5f8dbb4bce3856
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2024-01-31 12:21:48 +0200

    refactor(card): Remove column. See doc in Card component

--- LoginForm/app/src/main/java/com/example/app/loginform/LoginForm.kt
+++ LoginForm/app/src/main/java/com/example/app/loginform/LoginForm.kt
@@ -1,47 +1,37 @@
 package com.example.app.loginform
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Email
-import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material.icons.outlined.Visibility
 import androidx.compose.material.icons.outlined.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.app.loginform.ui.theme.LoginFormTheme
 
 @Composable
-fun RegistrationForm(modifier: Modifier = Modifier) {
+fun LoginForm() {
     var email by remember { mutableStateOf("") }
     var isValidEmail by remember { mutableStateOf(true) }
     var password by remember { mutableStateOf("") }
@@ -66,121 +56,89 @@ fun RegistrationForm(modifier: Modifier = Modifier) {
         isValidPassword = validatePassword(it)
     }
 
-    Column (
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(vertical = 36.dp)
-    ) {
-        Text(
-            text = "Login",
-            style = MaterialTheme.typography.headlineLarge,
-            color = MaterialTheme.colorScheme.primary,
-            modifier = Modifier.paddingFromBaseline(bottom = 42.dp)
+    Text(
+        text = "Login",
+        style = MaterialTheme.typography.headlineLarge,
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(24.dp)
         )
-        Row (
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Icon(
-                imageVector = Icons.Outlined.Email,
-                contentDescription = null,
-                tint = if (isValidEmail) MaterialTheme.colorScheme.primary else Color.Black,
-                modifier = Modifier.padding(horizontal = 10.dp)
-            )
-            OutlinedTextField(
-                value = email,
-                onValueChange = onEmailChange,
-                label = { Text(text = "Email") },
-                isError = !isValidEmail,
-                singleLine = true,
-                supportingText = {
-                    Text(text = if (!isValidEmail) "Invalid email address" else "")
-                },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Email,
-                    autoCorrect = false,
-                    // capitalization = KeyboardOptions.Capitalization.None,
-                    imeAction = ImeAction.Next,
-                ),
-                keyboardActions = KeyboardActions {
-                    validateEmail(email)
-                }
-            )
-        }
-       Row (
-           horizontalArrangement = Arrangement.Center,
-           verticalAlignment = Alignment.CenterVertically,
-           modifier = Modifier.fillMaxWidth()
-
-       ) {
-           Icon(
-               imageVector = Icons.Outlined.Lock,
-               contentDescription = null,
-               tint = if (isValidEmail) MaterialTheme.colorScheme.primary else Color.Black,
-               modifier = Modifier.padding(horizontal = 10.dp)
-           )
-           OutlinedTextField(
-               value = password,
-               onValueChange = onPasswordChange,
-               label = { Text(text = "Password") },
-               singleLine = true,
-               supportingText = {
-                   Text(text = if (!isValidPassword) "Password must be at least 8 characters" else "")
-               },
-               isError = !isValidPassword,
-               keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-               keyboardActions = KeyboardActions { validatePassword(password) },
-               visualTransformation =
-                    if (passwordHidden) PasswordVisualTransformation()
-                    else VisualTransformation.None,
-               trailingIcon = {
-                   val visibilityIcon = // Change icon based on password visibility state
-                       if (passwordHidden) Icons.Outlined.Visibility else Icons.Outlined.VisibilityOff
-                   // Localized description for accessibility services
-                   val description = if (passwordHidden) "Show password" else "Hide password"
-
-                   IconButton(
-                       onClick = { passwordHidden = !passwordHidden }
-                   ) {
-                       Icon(imageVector = visibilityIcon, contentDescription = description)
-                   }
-               }
-           )
-       }
-        Button(
-            onClick = { /*TODO*/ },
-            enabled = isValidEmail && isValidPassword,
-            // modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp),
-        ) {
-            Text(text = "Login")
-        }
-    }
-}
-
-@Composable
-fun LoginCard() {
-    Card (
-        modifier = Modifier
-            .fillMaxWidth()
-            //.align(Alignment.BottomCenter),
-//            .heightIn(max = 500.dp)
-//            .paddingFromBaseline(bottom = 0.dp)
-//            .padding(horizontal = 16.dp)
+    Row (
+        modifier = Modifier.padding(horizontal = 24.dp)
     ) {
-        RegistrationForm()
+        OutlinedTextField(
+            value = email,
+            onValueChange = onEmailChange,
+            label = { Text(text = "Email") },
+            isError = !isValidEmail,
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            supportingText = {
+                Text(text = if (!isValidEmail) "Invalid email address" else "")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                autoCorrect = false,
+                imeAction = ImeAction.Next,
+            ),
+            keyboardActions = KeyboardActions {
+                validateEmail(email)
+            },
+            trailingIcon = {
+                Icon(imageVector = Icons.Outlined.Email, contentDescription = "Email Icon")
+            }
+        )
+    }
+    Row (
+        modifier = Modifier.padding(horizontal = 24.dp)
+    ) {
+        OutlinedTextField(
+            value = password,
+            onValueChange = onPasswordChange,
+            label = { Text(text = "Password") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            supportingText = {
+                Text(text = if (!isValidPassword) "At least 8 characters" else "")
+            },
+            isError = !isValidPassword,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            keyboardActions = KeyboardActions { validatePassword(password) },
+            visualTransformation =
+            if (passwordHidden) PasswordVisualTransformation()
+            else VisualTransformation.None,
+            trailingIcon = {
+                val visibilityIcon = // Change icon based on password visibility state
+                    if (passwordHidden) Icons.Outlined.Visibility else Icons.Outlined.VisibilityOff
+                // Localized description for accessibility services
+                val description = if (passwordHidden) "Show password" else "Hide password"
+
+                IconButton(
+                    onClick = { passwordHidden = !passwordHidden }
+                ) {
+                    Icon(imageVector = visibilityIcon, contentDescription = description)
+                }
+            }
+        )
+    }
+    Button(
+        onClick = { /*TODO*/ },
+        enabled = isValidEmail && isValidPassword && email.isNotEmpty() && password.isNotEmpty(),
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp, vertical = 16.dp)
+    ) {
+        Text(text = "Login")
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun RegistrationFormPreview() {
-    Surface {
-        LoginCard()
-    }
-    LoginFormTheme {
-        // LoginCard()
+fun LoginCard(modifier: Modifier = Modifier, colors: Color) {
+    /*
+    By default, a Card wraps its content in a Column composable,
+    placing each item inside the card below one another.
+     */
+    Card (
+        modifier = modifier,
+        colors = CardDefaults.cardColors(colors)
+    ) {
+        LoginForm()
     }
 }
