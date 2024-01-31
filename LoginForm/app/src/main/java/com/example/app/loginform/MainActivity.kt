a59810ac939e09c053497ef7b4b49293a20cbbd8
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2024-01-31 12:23:00 +0200

    style(colors): Use own colors from UI Theme, update surface container and add preview

--- LoginForm/app/src/main/java/com/example/app/loginform/MainActivity.kt
+++ LoginForm/app/src/main/java/com/example/app/loginform/MainActivity.kt
@@ -3,27 +3,39 @@ package com.example.app.loginform
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.app.loginform.ui.theme.LoginFormTheme
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginFormTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    RegistrationForm(
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
+            Surface(
+                color = MaterialTheme.colorScheme.background
+            ) {
+                LoginCard(
+                    modifier = Modifier.padding(16.dp),
+                    colors = MaterialTheme.colorScheme.inverseOnSurface
+                )
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegistrationFormPreview() {
+    Surface (
+        color = MaterialTheme.colorScheme.background
+    ) {
+        LoginCard(
+            modifier = Modifier.padding(16.dp),
+            colors = MaterialTheme.colorScheme.inverseOnSurface
+        )
     }
 }
