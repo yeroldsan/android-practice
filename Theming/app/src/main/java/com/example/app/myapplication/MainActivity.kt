6c799256955eb211c40af7bd924e5e8d0e815c40
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2024-02-06 16:48:59 +0200

    style(layout): Add column and nested components

--- Theming/app/src/main/java/com/example/app/myapplication/MainActivity.kt
+++ Theming/app/src/main/java/com/example/app/myapplication/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.app.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp("Android")
                 }
             }
         }
@@ -30,17 +34,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(name: String, modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        OutlinedTextField(
+            value = "T",
+            onValueChange = { /* TODO */ },
+            modifier = Modifier
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+        ) {
+            Text(text = "Submit")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MyAppPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        MyApp("Android")
     }
 }
