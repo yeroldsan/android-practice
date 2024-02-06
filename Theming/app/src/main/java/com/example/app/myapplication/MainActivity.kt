15e9c1e1d3c7a93d4e44df8637646c5ba1320253
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2024-02-06 18:00:00 +0200

    refactor(imports): Remove unused imports

--- Theming/app/src/main/java/com/example/app/myapplication/MainActivity.kt
+++ Theming/app/src/main/java/com/example/app/myapplication/MainActivity.kt
@@ -9,12 +9,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
