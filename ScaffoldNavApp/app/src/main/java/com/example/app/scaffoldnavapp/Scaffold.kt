b05e0620d1fc0f5863389bc6fe69236dd9a96a2b
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2024-02-12 19:43:40 +0200

    feat(scaffold): Add top, bottom bar and text content to scaffold

---
+++ ScaffoldNavApp/app/src/main/java/com/example/app/scaffoldnavapp/Scaffold.kt
@@ -0,0 +1,33 @@
+package com.example.app.scaffoldnavapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ScaffoldNavAppScreen() {
+    Scaffold(
+        topBar = { MyTopBar() },
+        bottomBar = { MyBottomBar() },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text =
+                """
+                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
+                """.trimIndent(),
+            )
+        }
+    }
+
+}
