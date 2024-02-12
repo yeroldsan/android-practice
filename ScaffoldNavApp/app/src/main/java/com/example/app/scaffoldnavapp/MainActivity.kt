de034ca1de2ae571788edb654f875597dc55b69e
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2024-02-12 19:44:54 +0200

    feat(app): Add scaffold component to app and calls enable edge to edge function

--- ScaffoldNavApp/app/src/main/java/com/example/app/scaffoldnavapp/MainActivity.kt
+++ ScaffoldNavApp/app/src/main/java/com/example/app/scaffoldnavapp/MainActivity.kt
@@ -3,10 +3,10 @@ package com.example.app.scaffoldnavapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,6 +14,7 @@ import com.example.app.scaffoldnavapp.ui.theme.ScaffoldNavAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             ScaffoldNavAppTheme {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ScaffoldNavApp()
                 }
             }
         }
@@ -30,17 +31,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ScaffoldNavApp() {
+    ScaffoldNavAppScreen()
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ScaffoldNavAppTheme {
-        Greeting("Android")
-    }
+fun AppPreview() {
+    // ScaffoldNavAppTheme {
+        ScaffoldNavApp()
+    // }
 }
