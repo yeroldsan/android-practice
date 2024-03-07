6d282ee230c10b0d9db635ddc81a20319f46958c
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-07 20:48:14 +0200

    feat: Create app component

---
+++ BMIMVVM/app/src/main/java/com/example/app/bmimvvm/BmiApp.kt
@@ -0,0 +1,23 @@
+package com.example.app.bmimvvm
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.example.app.bmimvvm.ui.BmiScreen
+import com.example.app.bmimvvm.viewmodel.BmiViewModel
+
+@Composable
+fun BmiApp(viewModel: BmiViewModel = BmiViewModel()) {
+    val height by viewModel.height.collectAsState()
+    val weight by viewModel.weight.collectAsState()
+    val bmiResult by viewModel.result.collectAsState()
+
+    BmiScreen(
+        height = height,
+        weight = weight,
+        bmiResult = bmiResult,
+        setHeight = viewModel::setHeight,
+        setWeight = viewModel::setWeight,
+        calculateResult = viewModel::calculateBmi
+    )
+}
