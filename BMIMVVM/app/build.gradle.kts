32a1ade070078fd61782ceeffab8839751b5100d
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-07 20:43:53 +0200

    arch(app): Add dependencies

--- BMIMVVM/app/build.gradle.kts
+++ BMIMVVM/app/build.gradle.kts
@@ -51,6 +51,10 @@ android {
 
 dependencies {
 
+    // ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    // ViewModel utilities for Compose
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
