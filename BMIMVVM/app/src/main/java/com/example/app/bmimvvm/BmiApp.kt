f111256a0227a5d81bb85ac1adc9912efe129468
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-08 11:47:15 +0200

    fix(viewmodel): Use viewModel with BmIViewModel class

--- BMIMVVM/app/src/main/java/com/example/app/bmimvvm/BmiApp.kt
+++ BMIMVVM/app/src/main/java/com/example/app/bmimvvm/BmiApp.kt
@@ -4,10 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import com.example.app.bmimvvm.ui.BmiScreen
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.app.bmimvvm.viewmodel.BmiViewModel
 
 @Composable
-fun BmiApp(viewModel: BmiViewModel = BmiViewModel()) {
+fun BmiApp(viewModel: BmiViewModel = viewModel()) {
     val height by viewModel.height.collectAsState()
     val weight by viewModel.weight.collectAsState()
     val bmiResult by viewModel.result.collectAsState()
