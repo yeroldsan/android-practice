a010156c28348b2160b1ae1807ccd8edbbbb259b
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-11 18:05:43 +0200

    feat(build): Add retrofit and gson from retrofit

--- ToDo/app/build.gradle.kts
+++ ToDo/app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 
 dependencies {
 
+    // Retrofit and GSON migrated to version catalog
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
