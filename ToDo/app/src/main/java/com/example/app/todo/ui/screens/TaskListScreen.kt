90d7dcbe186859fba9f211ebe1a79875d1916131
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-11 11:17:51 +0200

    feat(app): Add task card component to render list of tasks

--- ToDo/app/src/main/java/com/example/app/todo/ui/screens/TaskListScreen.kt
+++ ToDo/app/src/main/java/com/example/app/todo/ui/screens/TaskListScreen.kt
@@ -1,22 +1,43 @@
 package com.example.app.todo.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.app.todo.viewmodel.TodoViewModel
 
 @Composable
-fun TaskListScreen() {
-    
+fun TaskListScreen(vModel: TodoViewModel = viewModel()) {
+    val tasks by vModel.tasks.collectAsState()
+    TaskList(tasks)
+}
+
+@Preview
+@Composable
+fun TaskListPreview() {
+    TaskListScreen()
 }
 
 @Composable
 fun TaskList(tasks: List<String>) {
     LazyColumn (
+        modifier = Modifier
+            .fillMaxWidth(),
         contentPadding = PaddingValues(
             horizontal = 16.dp,
             vertical = 8.dp
@@ -24,9 +45,28 @@ fun TaskList(tasks: List<String>) {
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
         items(tasks) { task ->
-            Card {
-                Text(text = task)
-            }
+            TaskCard(task)
+        }
+    }
+}
+
+@Composable
+fun TaskCard(task: String) {
+    Card (
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = task,
+                style = MaterialTheme.typography.bodyLarge,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+            )
         }
     }
 }
