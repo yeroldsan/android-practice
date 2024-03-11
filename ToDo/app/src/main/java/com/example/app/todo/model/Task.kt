9f117771ee96e6efece394bf9e6916f6f59f42f4
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-11 18:06:24 +0200

    feat(model): Create task model and repository

---
+++ ToDo/app/src/main/java/com/example/app/todo/model/Task.kt
@@ -0,0 +1,39 @@
+package com.example.app.todo.model
+
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+data class Task(
+    // Example of how to rename a field
+    // @SerializedName("userId") var uID: Int,
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val completed: Boolean
+)
+
+interface TaskRepository {
+    @GET("todos")
+    suspend fun getTasks(): List<Task>
+
+    @GET("todos/{id}")
+    suspend fun getTask(id: Int): Task
+
+    // Create an instance of TaskRepository
+    companion object {
+        private var instance: TaskRepository? = null
+        fun getInstance(): TaskRepository {
+            if (instance == null) {
+                instance = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                    .create(TaskRepository::class.java)
+            }
+            return instance!!
+        }
+    }
+}
