f04d15e1c7ef04ed3d8cefb66fd81ceb6a546acf
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-11 18:07:32 +0200

    feat(viewmodel): Retrieve tasks list from model repository

--- ToDo/app/src/main/java/com/example/app/todo/viewmodel/TodoViewModel.kt
+++ ToDo/app/src/main/java/com/example/app/todo/viewmodel/TodoViewModel.kt
@@ -1,14 +1,30 @@
 package com.example.app.todo.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.app.todo.model.Task
+import com.example.app.todo.model.TaskRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class TodoViewModel: ViewModel() {
-    private val _tasks = MutableStateFlow<List<String>>(emptyList())
-    val tasks: StateFlow<List<String>> = _tasks
+    private val _tasks = MutableStateFlow<List<Task>>(emptyList())
+    val tasks: StateFlow<List<Task>> = _tasks
 
     init {
-        _tasks.value = listOf("Task 1", "Task 2", "Task 3", "Task 4", "Task 5")
+        getTasks()
+//        _tasks.value = listOf(Task(1, 1,"Task 1", true), Task(1, 2,"Task 2", false))
+    }
+
+    private fun getTasks() {
+        viewModelScope.launch {
+            try {
+                val tasks = TaskRepository.getInstance()
+                _tasks.value = tasks.getTasks()     // String list of task's title:  .map { it.title }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 }
